28d31a6ea66b1b6baae36adfa39e25802697a1b3
xiaolin <462441355@qq.com>
2021-11-25 16:23:48 +0800

    * name 强校验

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs')
-const path = require('path')
-const childProcess = require('child_process')
-const versionDir = path.join(__dirname, '../version');
+var fs = require('fs')
+var path = require('path')
+var childProcess = require('child_process')
+var versionDir = path.join(__dirname, '../version');
 
-const initVersion = '1.0.0.0';
+var initVersion = '1.0.0.0';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let response = {
+    success: true
+  }
   const name = req.query.name;
+
+  if (!name) {
+    return res.json({
+      success: false,
+      errorMessage: 'name必传'
+    })
+    
+  }
   // 确认要更新的版本号 每一个应用独立一个文件管理版本
   // 创建一个文件， 默认的版本号
+
+  // 创建一个当前应用的路径
+  const currentUrl = path.join(versionDir, name);
+  // console.log(currentUrl)
 
   res.render('index', { title: 'Express' });
 });
